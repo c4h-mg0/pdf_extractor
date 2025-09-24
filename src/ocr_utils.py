2aeed8afc9f4331d304a853589a8f87cb2f555db
Cassio M. B. Magalhães <cmmagalhaes@mirassol.sp.gov.br>
2025-09-24 13:16:45 -0300

    test 0.35

--- src/ocr_utils.py
+++ src/ocr_utils.py
@@ -3,25 +3,33 @@ import os
 from pdf2image import convert_from_path
 from PIL import Image, ImageOps
 import pytesseract
+import fitz
+import io
 
 
-def pdf_ocr_text(pdf_path, dpi=300, lang="por"):
-    """Converte PDF em texto via OCR, com pré-processamento."""
+
+def pdf_ocr_text(pdf_path, dpi=400, lang="por"):
     texto_total = []
-    pages = convert_from_path(pdf_path, dpi=dpi)
+    doc = fitz.open(pdf_path)
 
-    for page_num, page in enumerate(pages):
+    for page_num, page in enumerate(doc):
+        rect = page.rect
+        top40 = fitz.Rect(0, 0, rect.width, rect.height * 0.35)
+
+        # Renderizar só a área cortada
+        pix = page.get_pixmap(clip=top40, dpi=dpi)
+        img = Image.open(io.BytesIO(pix.tobytes("png")))
+
         # Pré-processamento
-        img = page.convert("L")
+        img = img.convert("L")
         img = ImageOps.autocontrast(img, cutoff=1)
         img = img.point(lambda x: 0 if x < 140 else 255)
 
         # OCR
-        page_text = pytesseract.image_to_string(
-            img, lang=lang, config="--psm 6 --oem 1"
-        )
+        page_text = pytesseract.image_to_string(img, lang=lang, config="--psm 6 --oem 1")
         texto_total.append(page_text)
 
+    doc.close()
     return "\n".join(texto_total)
 
 
